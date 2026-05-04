b2bdc34677923604fcbf26e55ed362b29eca893b
Mengru <mengru@MengrudeMacBook-Air.local>
2026-05-04 18:59:50 +0800

    feat: expose settings IPC methods in preload

--- packages/app/electron/preload.ts
+++ packages/app/electron/preload.ts
@@ -4,4 +4,9 @@ contextBridge.exposeInMainWorld("electronAPI", {
   selectDirectory: () => ipcRenderer.invoke("select-directory"),
   startServer: (projectRoot: string) =>
     ipcRenderer.invoke("start-server", projectRoot),
+  getSettings: () => ipcRenderer.invoke("get-settings"),
+  saveSettings: (settings: unknown) =>
+    ipcRenderer.invoke("save-settings", settings),
+  getSupportedProviders: () =>
+    ipcRenderer.invoke("get-supported-providers"),
 });
